519bcad90c289e4b50f1223778c2fc94e6c77830
AFPINTO <AFPINTO@48447D3.corp.netledger.com>
2022-08-24 16:08:28 +0200

    basic functionality

--- nslogify.js
+++ nslogify.js
@@ -1,26 +1,48 @@
 console.log('extension', 'init');
-// document.body.style.border = '50px solid red';
 
-// ! DOM ELEMENTS
+// ? DOM ELEMENTS
 
-// const executionLogTab = document.getElementById('executionloglnk');
-// const scriptNotes = document.getElementById('scriptnote__div');
-// const logDiv = document.getElementById('executionlog_div');
-// console.log('logDiv', logDiv);
+const executionLogTab = document.getElementById('executionloglnk');
+const refreshBtn = document.getElementById('refreshscriptnote');
 
-// const classTest = document.getElementsByClassName('uir-list-row-tr');
-// console.log('class', classTest);
-
-// executionLogTab.addEventListener('click', function () {
-// 	console.log('executionLogTab', 'event');
-// 	const scriptNotesTable = document.getElementById('scriptnote__tab');
-// 	console.log('scriptNotesTable', scriptNotesTable);
-// });
-
-// logDiv.addEventListener('submit', function () {
-// 	console.log('ok');
-// });
-
-window.addEventListener('load', function (event) {
-	console.log('page is fully loaded');
+executionLogTab.addEventListener('click', function () {
+	getLogs();
 });
+
+refreshBtn.addEventListener('click', function () {
+	getLogs();
+});
+
+const getLogs = () => {
+	setTimeout(() => {
+		const scriptNotes = document.getElementById('scriptnote__tab');
+		// ! tBody is the table containing the logs!
+		const tBody = scriptNotes.children[1];
+		const titleCol = 3;
+		const detailsCol = 7;
+		const logs = tBody.childNodes;
+
+		console.log(logs);
+		console.log(logs.length);
+
+		for (let i = 0; i < logs.length / 2; i++) {
+			let row = document.getElementById(`scriptnoterow${i}`);
+			let details = row.children[detailsCol].textContent;
+
+			const detailsObj = JSON.parse(details);
+			const detailsObjPrint = JSON.stringify(detailsObj, null, 4);
+
+			row.children[detailsCol].innerHTML = '<pre><code>' + detailsObjPrint + '</pre></code>';
+		}
+
+		// logs.forEach((log) => {
+		// 	let title = log.cells[titleCol].textContent;
+		// 	let details = log.cells[detailsCol].textContent;
+
+		// 	const detailsObj = JSON.parse(details);
+		// 	const detailsObjPrint = JSON.stringify(detailsObj, null, 4);
+
+		// 	log.cells[detailsCol].innerHTML = '<pre><code>' + detailsObjPrint + '</pre></code>';
+		// });
+	}, 600);
+};
