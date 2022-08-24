7caf45e91950a25a778840499810de64e1e1fcb4
AFPINTO <AFPINTO@48447D3.corp.netledger.com>
2022-08-24 09:33:30 +0200

    init

---
+++ nslogify.js
@@ -0,0 +1,26 @@
+console.log('extension', 'init');
+// document.body.style.border = '50px solid red';
+
+// ! DOM ELEMENTS
+
+// const executionLogTab = document.getElementById('executionloglnk');
+// const scriptNotes = document.getElementById('scriptnote__div');
+// const logDiv = document.getElementById('executionlog_div');
+// console.log('logDiv', logDiv);
+
+// const classTest = document.getElementsByClassName('uir-list-row-tr');
+// console.log('class', classTest);
+
+// executionLogTab.addEventListener('click', function () {
+// 	console.log('executionLogTab', 'event');
+// 	const scriptNotesTable = document.getElementById('scriptnote__tab');
+// 	console.log('scriptNotesTable', scriptNotesTable);
+// });
+
+// logDiv.addEventListener('submit', function () {
+// 	console.log('ok');
+// });
+
+window.addEventListener('load', function (event) {
+	console.log('page is fully loaded');
+});
